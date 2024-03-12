db519b1eef21aef77f17a3aaef75525a68b85243
abdAllah <150131028+abdAllahBelhouari@users.noreply.github.com>
2024-03-12 11:36:02 +0100

    Update autoload.php

--- public/autoload.php
+++ public/autoload.php
@@ -1,23 +1,21 @@
 <?php
-$dir = '../models/';
-$inc = scandir($dir);
-if($inc){
-    foreach($inc as $include){
-        if($include != '' && $include != "." && $include != ".."){
-            require_once($dir.$include); 
-                     
-           
+    $dir = '../models/';
+    $inc = scandir($dir);
+    if($inc){
+        foreach($inc as $include){
+            if($include != '' && $include != "." && $include != ".."){
+                require_once($dir.$include);
+            }
         }
     }
-}
-$dir = '../controllers/';
-$inc = scandir($dir);
-if($inc){
-    foreach($inc as $include){
-        if($include != '' && $include != "." && $include != ".."){
-            require_once($dir.$include);            
-        }
-    }
-}
 
+    $dir = '../controllers/';
+    $inc = scandir($dir);
+    if($inc){
+        foreach($inc as $include){
+            if($include != '' && $include != "." && $include != ".."){
+                require_once($dir.$include);            
+            }
+        }
+    }
 ?>
